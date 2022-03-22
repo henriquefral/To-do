b1a0c9098b3a57ba9ca5464a87df0b3e62a648af
gessilt <henriquefreitas2710@gmail.com>
2022-03-22 10:14:27 -0300

    feat: new color for title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
-        <h1 class="text-white-900 display-4"> To do </h1>
+        <h1 class="title display-4"> To do </h1>
         <todo-component> </todo-component>
     </div>
 @endsection
